14b41129f04374625bd1219ad90b4ca5e10ec08c
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-12-03 14:09:31 +0530

    Add files via upload

---
+++ fibonacci2.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a range"))
+first_number=1
+second_number=0
+third_number=0
+while(third_number<n):
+    print(third_number,end=" ")
+    third_number=first_number+second_number
+    first_number=second_number
+    second_number=third_number
